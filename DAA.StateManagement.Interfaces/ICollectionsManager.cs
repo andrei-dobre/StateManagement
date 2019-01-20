7294a084d049af9493072eb96ae3cf2778e2703b
Andrei-Alexandru Dobre <andrei.dobre@studyportals.com>
2019-01-20 13:25:07 +0100

    Sketched the CollectionsManager class

---
+++ DAA.StateManagement.Interfaces/ICollectionsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAA.StateManagement.Interfaces
+{
+    public interface ICollectionsManager<TData>
+        where TData : IData
+    {
+        Task FillCollectionAsync(ICollection<TData> collection, INonTerminalDescriptor descriptor);
+        void DropCollection(ICollection<TData> collection);
+    }
+}
---
+++ DAA.StateManagement/CollectionsManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAA.Helpers;
+using DAA.StateManagement.Interfaces;
+
+namespace DAA.StateManagement
+{
+    public class CollectionsManager<TData> : ICollectionsManager<TData>
+        where TData : IData
+    {
+        private IDataPool<TData> DataPool { get; }
+        private IStateEventsAggregator<TData> StateEventsAggregator { get; }
+
+        private IDictionary<INonTerminalDescriptor, ICollection<ICollection<TData>>> CollectionsByDescriptor { get; }
+        private IDictionary<ICollection<TData>, INonTerminalDescriptor> DescriptorByCollection { get; }
+
+
+        public CollectionsManager(IDataPool<TData> dataPool, IStateEventsAggregator<TData> stateEventsAggregator)
+        {
+            DataPool = dataPool;
+            StateEventsAggregator = stateEventsAggregator;
+
+            CollectionsByDescriptor = new Dictionary<INonTerminalDescriptor, ICollection<ICollection<TData>>>();
+            DescriptorByCollection = new Dictionary<ICollection<TData>, INonTerminalDescriptor>();
+
+            StateEventsAggregator.CompositionChangedEvent += WhenCompositionChanged;
+        }
+
+
+        public async Task FillCollectionAsync(ICollection<TData> collection, INonTerminalDescriptor descriptor)
+        {
+            RegisterCollection(collection, descriptor);
+            FillCollectionWithData(collection, descriptor);
+        }
+
+        private void FillCollectionWithData(ICollection<TData> collection, INonTerminalDescriptor descriptor)
+        {
+            ClearCollection(collection);
+            DataPool.Retrieve(descriptor).ForEach(collection.Add);
+        }
+
+        public void DropCollection(ICollection<TData> collection)
+        {
+            if (CollectionIsRegistered(collection))
+            {
+                ClearCollection(collection);
+                DropCollection(collection, GetDescriptor(collection));
+            }
+        }
+
+        public virtual void WhenCompositionChanged(object sender, INonTerminalDescriptor descriptor)
+        {
+            FindAffectedCollections(descriptor).ForEach(UpdateCollection);
+        }
+
+        public virtual void UpdateCollection(ICollection<TData> collection)
+        {
+            var descriptor = GetDescriptor(collection);
+            var data = DataPool.Retrieve(descriptor);
+
+            collection.Update(data);
+        }
+
+        public virtual void RegisterCollection(ICollection<TData> collection, INonTerminalDescriptor descriptor)
+        {
+            if (!CollectionIsRegistered(collection))
+            {
+                GetCollectionsForDescriptor(descriptor).Add(collection);
+                DescriptorByCollection.Add(collection, descriptor);
+            }
+        }
+
+        public virtual IEnumerable<ICollection<TData>> FindAffectedCollections(INonTerminalDescriptor changedDescriptor)
+        {
+            return FindIntersectingDescriptors(changedDescriptor).SelectMany(_ => CollectionsByDescriptor[_]).ToArray();
+        }
+
+        public virtual IEnumerable<INonTerminalDescriptor> FindIntersectingDescriptors(INonTerminalDescriptor changedDescriptor)
+        {
+            return CollectionsByDescriptor.Keys.Where(_ => _.Intersects(changedDescriptor));
+        }
+
+        public virtual INonTerminalDescriptor GetDescriptor(ICollection<TData> collection)
+        {
+            return DescriptorByCollection[collection];
+        }
+
+        public virtual void DropCollection(ICollection<TData> collection, INonTerminalDescriptor descriptor)
+        {
+            DescriptorByCollection.Remove(collection);
+            CollectionsByDescriptor[descriptor].Remove(collection);
+        }
+
+        public virtual bool CollectionIsRegistered(ICollection<TData> collection)
+        {
+            return DescriptorByCollection.ContainsKey(collection);
+        }
+
+        public virtual void ClearCollection(ICollection<TData> collection)
+        {
+            collection.RemoveClear();
+        }
+
+        public virtual ICollection<ICollection<TData>> GetCollectionsForDescriptor(INonTerminalDescriptor descriptor)
+        {
+            if (!CollectionsByDescriptor.ContainsKey(descriptor))
+            {
+                CollectionsByDescriptor[descriptor] = new List<ICollection<TData>>();
+            }
+
+            return CollectionsByDescriptor[descriptor];
+        }
+    }
+}
