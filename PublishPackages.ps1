e0eac75dfe4861ce2906bedee197da8eb7cdbfa6
Andrei-Alexandru Dobre <andrei@internetwerk.nu>
2021-07-23 20:23:48 +0200

    Version;

--- PublishPackages.ps1
+++ PublishPackages.ps1
@@ -1,2 +1,2 @@
-dotnet nuget push '.\DAA.StateManagement\bin\Release\DAA.StateManagement.1.0.9.nupkg' -s D:\NuGet
+dotnet nuget push '.\DAA.StateManagement\bin\Release\DAA.StateManagement.1.0.10.nupkg' -s D:\NuGet
 dotnet nuget push '.\DAA.StateManagement.Interfaces\bin\Release\DAA.StateManagement.Interfaces.1.0.13.nupkg' -s D:\NuGet
